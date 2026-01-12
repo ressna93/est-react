0076515fa5ab0d117da87c6e099734ad437a0041
youngjongkim <umaru19930407@gmail.com>
2026-01-12 14:49:39 +0900

    feat : Props Drilling의 문제점

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import React, { useState } from 'react';
+
 import './App.css';
+import { UserInfo } from './UserInfo';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [name, setName] = useState<string>('홍길동');
+  const [age, setAge] = useState<number>(20);
 
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setName(event.target.value);
+  };
+  const handleAgeChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setAge(Number(event.target.value));
+  };
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <input
+        className="border p-1.5"
+        type="text"
+        placeholder="이름을 입력하세요."
+        onChange={handleNameChange}
+      />
+      <input
+        className="border p-1.5 ml-2"
+        type="number"
+        placeholder="나이를 입력하세요."
+        onChange={handleAgeChange}
+      />
+      <UserInfo name={name} age={age} />
     </>
   );
 }
